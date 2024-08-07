791d80e1bd0548abac797f39e7e7671a420e0aa2
danielW3ll <hello@wellermann.de>
2024-08-08 00:11:41 +0200

    chore: Update layout and formatting in index.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -14,70 +14,75 @@ const drawer = ref(true)
 // })
 </script>
 <template>
-    <v-app>
-        <!-- <v-navigation-drawer :width="250" mobile-breakpoint="md" style="top: 0px; bottom: 0px;"> -->
-        <v-navigation-drawer v-model="drawer" mobile-breakpoint="md" :width="250" style="top: 0px; bottom: 0px;">
-            <v-row class="align-center justify-center flex-column fill-height pt-8 mx-0">
-                <h2 class="text-h2 font-weight-black text-primary mb-2">DW</h2>
-                <div class="text-h5 mb-2">Daniel Wellermann</div>
-                <p class="font-weight-thin mb-5">BIM-Magier und Maker</p>
-                <!-- About Me -->
-                <div class="my-1">
-                    <v-btn variant="text" class="font-weight-light" to="/">START</v-btn>
-                </div>
-                <!-- Blog & Docs -->
-                <div class="my-1">
-                    <v-btn variant="text" class="font-weight-light">Blog & Docs</v-btn>
-                </div>
-                <!-- W-Helfer (utilities für den Alltag ) -->
-                <div class="my-1">
-                    <v-btn variant="text" class="" to="/about">
-                        <template v-slot:append>
-                            <v-badge class="ms-2 mb-4" color="amber" content="APP">
-                            </v-badge>
-                        </template>
-                        <span class="text-primary font-weight-bold">W</span><span>-Helfer</span>
-                    </v-btn>
-                </div>
-                <!-- Naturbaukasten App für Ökobaudat ?  -->
-                <div class="my-1" hidden>
-                    <v-btn variant="text" class="">
-                        <template v-slot:append>
-                            <v-badge class="ms-2 mb-4" color="amber" content="APP">
-                            </v-badge>
-                        </template>
-                        <span>Naturbaukasten.de</span>
-                    </v-btn>
-                </div>
-
-                <!-- Kontakt -->
-                <div class="my-1">
-                    <v-btn variant="text" class="font-weight-light text-amber">Get in Touch!</v-btn>
-                </div>
-                <!-- Footer  Github and Socials -->
-                <div class="mt-auto mb-4">
-                    <v-btn icon href="https://github.com/danielW3ll" target="_blank" class="mx-3">
-                        <v-icon>mdi-github</v-icon>
-                    </v-btn>
-                    <v-btn icon href="https://de.linkedin.com/in/wellermann" target="_blank" class="mx-3">
-                        <v-icon>mdi-linkedin</v-icon>
-                    </v-btn>
-                    <v-btn icon href="https://www.instagram.com/daniel_wellermann" target="_blank" class="mx-3">
-                        <v-icon>mdi-instagram</v-icon>
-                    </v-btn>
-                </div>
-            </v-row>
-        </v-navigation-drawer>
-        <v-app-bar v-if="$vuetify.display.smAndDown" collapse elevation="7" class="">
-            <template v-slot:prepend>
-
-                <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-app>
+    <!-- <v-navigation-drawer :width="250" mobile-breakpoint="md" style="top: 0px; bottom: 0px;"> -->
+    <v-navigation-drawer v-model="drawer" mobile-breakpoint="md" :width="250" style="top: 0px; bottom: 0px;">
+      <v-row class="align-center justify-center flex-column fill-height pt-8 mx-0">
+        <h2 class="text-h2 font-weight-black text-primary mb-2">DW</h2>
+        <div class="text-h5 mb-2">Daniel Wellermann</div>
+        <p class="font-weight-thin mb-5">BIM-Magier und Maker</p>
+        <!-- About Me -->
+        <div class="my-1">
+          <v-btn variant="text" class="font-weight-light" to="/">START</v-btn>
+        </div>
+        <!-- Blog & Docs -->
+        <div class="my-1">
+          <v-btn variant="text" class="font-weight-light" disabled>Blog & Docs</v-btn>
+        </div>
+        <!-- W-Helfer (utilities für den Alltag ) -->
+        <div class="my-1">
+          <v-btn variant="text" class="" to="/about" disabled>
+            <template v-slot:append>
+              <v-badge class="ms-2 mb-4" color="amber" content="APP">
+              </v-badge>
             </template>
-            <v-toolbar-title>Wellermann</v-toolbar-title>
-        </v-app-bar>
-        <v-main>
+            <span class="text-primary font-weight-bold">W</span><span>-Helfer</span>
+          </v-btn>
+        </div>
+        <!-- Naturbaukasten App für Ökobaudat ?  -->
+        <div class="my-1" hidden>
+          <v-btn variant="text" class="" disabled>
+            <template v-slot:append>
+              <v-badge class="ms-2 mb-4" color="amber" content="APP">
+              </v-badge>
+            </template>
+            <span>Naturbaukasten.de</span>
+          </v-btn>
+        </div>
 
-            <slot />
-        </v-main>
-    </v-app>
+        <!-- Kontakt -->
+        <div class="my-1">
+          <v-btn variant="text" class="font-weight-light text-amber" disabled>Get in Touch!</v-btn>
+        </div>
+        <!-- Footer  Github and Socials -->
+        <div class="mt-auto mb-4">
+          <v-btn icon href="https://github.com/danielW3ll" target="_blank" class="mx-3">
+            <v-icon>mdi-github</v-icon>
+          </v-btn>
+          <v-btn icon href="https://de.linkedin.com/in/wellermann" target="_blank" class="mx-3">
+            <v-icon>mdi-linkedin</v-icon>
+          </v-btn>
+          <v-btn icon href="https://www.instagram.com/daniel_wellermann" target="_blank" class="mx-3">
+            <v-icon>mdi-instagram</v-icon>
+          </v-btn>
+        </div>
+      </v-row>
+    </v-navigation-drawer>
+    <!-- <v-app-bar v-if="$vuetify.display.smAndDown" collapse elevation="7" class="">
+      <template v-slot:prepend>
+
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+      <v-toolbar-title>Wellermann</v-toolbar-title>
+    </v-app-bar> -->
+
+    <!-- Floating Burger Button with v-fab-->
+
+    <v-main>
+
+      <v-fab @click="drawer = !drawer" :active="$vuetify.display.smAndDown" icon="mdi-menu" location="top right" variant="tonal" app>
+      </v-fab>
+      <slot />
+    </v-main>
+  </v-app>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,52 +5,50 @@ definePageMeta({
 })
 </script>
 <template>
-    <v-app>
-        <!-- Hero -->
-        <section>
-            <v-responsive class="">
-                <!-- background  -->
-                <v-img width="100%" cover style="height: 100vh;" src="/public/World-map-image.png">
-                    <!-- <v-img cover src="/public/hero.png" style="height: 100vh;"> -->
-                    <v-row class="align-end justify-end fill-height text-right pa-md-5 pa-3 mx-0">
-                        <div>
-                            <div class="text-h4 text-uppercase">Daniel Wellermann</div>
-                            <h1 class="text-h2 text-primary text-uppercase">BIM-Magier und Maker</h1>
-                        </div>
-                    </v-row>
-                </v-img>
-                <!-- content
+  <v-app>
+    <!-- Hero -->
+    <section>
+      <v-responsive class="">
+        <!-- background  -->
+        <v-img width="100%" cover style="height: 100vh;" src="/public/World-map-image.png">
+          <!-- <v-img cover src="/public/hero.png" style="height: 100vh;"> -->
+          <v-row class="align-end justify-end fill-height text-right pa-md-5 pa-3 mx-0">
+            <div>
+              <div class="text-h4 text-uppercase">Daniel Wellermann</div>
+              <h1 class="text-h2 text-primary text-uppercase">BIM-Magier und Maker</h1>
+            </div>
+          </v-row>
+        </v-img>
+        <!-- content
                         <div class="v-row align-end justify-end fill-height text-right pa-md-5 pa-3 mx-0"><div><div class="text-h2 text-uppercase">John Smith</div><h1 class="text-h1 text-primary text-uppercase">Front-end Developer</h1></div></div>
 
                         -->
-            </v-responsive>
-        </section>
-        <!-- About Me -->
+      </v-responsive>
+    </section>
+    <!-- About Me -->
 
-        <section id="about" class="v-row mt-4">
-            <v-responsive>
-                <v-container fluid>
-                    <v-row class="fill-height mx-0 justify-space-between">
-                        <v-col cols="6" class="pa-5">
-                            <h4 class="text-h4 text-uppercase mb-8">Über Mich</h4>
-                            <p>Ich bin ein BIM-Magier und Maker. Ich liebe es, die Welt zu erkunden und neue Dinge
-                                zu
-                                lernen. Ich bin ein leidenschaftlicher Programmierer und Designer und liebe es, mit
-                                neuen Technologien zu experimentieren. Ich bin ein großer Fan von Open Source und
-                                liebe
-                                es, mein Wissen mit anderen zu teilen. Ich bin ein großer Fan von Open Source und
-                                liebe
-                                es, mein Wissen mit anderen zu teilen.</p>
-                        </v-col>
-                        <v-col cols="6" class="pa-5 ">
-                            <v-img src="/public/ich.jpeg" max-width="30rem" class="rounded-lg elevation-5 ms-auto" />
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-responsive>
-        </section>
-        <!-- Was mich z.Z Umtreibt -->
-        <section>
-        </section>
-    </v-app>
+    <section id="about" class="v-row mt-4">
+      <v-responsive>
+        <v-container>
+          <v-row class="fill-height mx-0" justify="center">
+            <v-col class="pa-5" cols="12" md="6">
+              <h4 class="text-h4 text-uppercase mb-8">Moin</h4>
+              <p>Hier erfährst Du mehr über meine Leidenschaft im Bereich
+                Building Information Modeling (BIM),
+                Softwareentwicklung und Maker-Projekte.</p>
+              <br>
+              <p>Herzlich Willkommen</p>
+              <i>Daniel Wellermann</i>
+            </v-col>
+            <v-col class="pa-5 d-flex justify-center align-center">
+              <v-img src="/public/ich.jpeg" max-width="30rem" class="rounded-lg elevation-5 " />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-responsive>
+    </section>
+    <!-- Was mich z.Z Umtreibt -->
+    <section>
+    </section>
+  </v-app>
 </template>
